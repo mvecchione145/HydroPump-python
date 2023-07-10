07fa16297ef0be674191eda7b473e67028bec64e
Michael Vecchione <51305946+mvecchione145@users.noreply.github.com>
2023-07-09 21:51:20 -0400

    bump

--- hydropump/__init__.py
+++ hydropump/__init__.py
@@ -1,6 +1,6 @@
 """Hydro Pump is a framework for managing configurations for your processes."""
 
-version = "0.1.0"
+version = "0.1.1"
 
 from .backend import Backend, FileSystemBackend
 from .instruction import Instruction
